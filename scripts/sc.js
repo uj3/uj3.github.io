a3c75db5685eef042695fa0b92402bced80230c1
uj3 <jaiswal1996ujjawal@gmail.com>
2017-12-05 22:47:12 +0530

    Add files via upload

---
+++ scripts/sc.js
@@ -0,0 +1,17 @@
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" uj-red", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " uj-red";
+}
+document.getElementById("myLink").click();
+
+
+  
---
+++ service-worker.js
@@ -0,0 +1,46 @@
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/me.html',
+  '/scripts/app.js',
+  '/scripts/sc.js',
+  '/styles/sd.css',
+  '/images/download.jpg',
+  '/images/h.jpg',
+  '/images/images.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
